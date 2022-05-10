0eaac496b5d79c78f6492c09e4b2d7a1f3dc0a23
mirfansyarif <muhammadis@student.ce.undip.ac.id>
2022-05-10 13:35:10 +0700

    delete-routing-edit

--- src/app/owner/owner-routing.module.ts
+++ src/app/owner/owner-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateComponent } from './create/create.component';
-import { EditComponent } from './edit/edit.component';
 import { IndexComponent } from './index/index.component';
 
 const routes: Routes = [
@@ -13,10 +12,6 @@ const routes: Routes = [
         path: 'create',
         component: CreateComponent
       },
-      {
-        path: 'edit',
-        component: EditComponent
-      }
     ]
   }
 ]
